1994315687e4c2e76de71549458b7becb9b98c37
DerrickJames <dmushangi@gmail.com>
2014-11-23 22:46:54 +0300

    Added comments collection, publish/subscribe and fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,43 @@
 if (Posts.find().count() === 0) {
+  var now = new Date().getTime();
+
+  // create two users
+  var armyId = Meteor.users.insert({
+    profile: { name: 'Army James' }
+  });
+
+  var army = Meteor.users.findOne(armyId);
+
+  var naaId = Meteor.users.insert({
+    profile: { name: 'Amy Naa' }
+  });
+
+  var naa = Meteor.users.findOne(naaId);
+
+  var micromId = Posts.insert({
+    title: 'Introducing Microm',
+    userId: naa._id,
+    author: naa.profile.name,
+    url: 'http://microm.com',
+    submitted: new Date(now - 7 * 3600 * 1000)
+  });
+
+  Comments.insert({
+    postId: micromId,
+    userId: army._id,
+    author: army.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: 'Interesting project Naa, can I get involved?'
+  });
+
+  Comments.insert({
+    postId: micromId,
+    userId: naa._id,
+    author: naa.profile.name,
+    submitted: new Date(now - 3 * 3600 * 1000),
+    body: 'You sure can Army!'
+  });
+  
   Posts.insert({
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope/'
